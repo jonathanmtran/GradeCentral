74543b0f39d9acd9f08401019febca95c464f709
Jonathan M. Tran <jmt@jonathanmtran.com>
2013-07-31 00:10:04 -0700

    Avoid dynamic SQL with concatenated input.

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -7,32 +7,38 @@ Partial Class _Default
     Protected Sub btnLogin_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnLogin.Click
         Dim connAuthenticate As New SqlConnection
         connAuthenticate.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings("GradesCentralConnectionString").ConnectionString
-        connAuthenticate.Open()
-        Dim strSQL As String = "SELECT * FROM Students WHERE Username = '" & tbUsername.Text & "' and Password = '" & tbPassword.Text & "'"
-        Dim drAuthenticate As SqlDataReader
-        Dim cmdAuthenticate As New SqlCommand(strSQL, connAuthenticate)
-        drAuthenticate = cmdAuthenticate.ExecuteReader()
-        If drAuthenticate.Read() Then
-            lblLogin.Text = "Welcome! "
-            sqlDSGradesUser.SelectCommand = "SELECT * FROM Grades WHERE StudentID = " & drAuthenticate.Item("StudentID")
-            sqlDSGradesUser.DataBind()
-            gvGradesUser.DataBind()
-            sqlDSStudentsAll.SelectCommand = "SELECT * FROM Students"
-            gvStudentsAll.DataBind()
-            sqlDSGradesAll.SelectCommand = "SELECT * FROM Grades"
-            gvGradesAll.DataBind()
-            gvGradesUser.Visible = True
-            gvStudentsAll.Visible = True
-            gvGradesAll.Visible = True
-            lblGradesUser.Visible = True
-            lblStudentsAll.Visible = True
-            lblGradesAll.Visible = True
-            lblSQLStatement.Visible = True
-            lblSQL.Text = strSQL
-        Else
-            lblLogin.Text = "Invalid credential."
-        End If
-    End Sub
+		connAuthenticate.Open()
+
+		' Dim strSQL As String = "SELECT * FROM Students WHERE Username = '" & tbUsername.Text & "' and Password = '" & tbPassword.Text & "'"
+		Dim strSQL As String = "SELECT * FROM Students WHERE Username = @username and Password = @password"
+		Dim drAuthenticate As SqlDataReader
+		Dim cmdAuthenticate As New SqlCommand(strSQL, connAuthenticate)
+
+		cmdAuthenticate.Parameters.Add(New SqlParameter("@username", tbUsername.Text))
+		cmdAuthenticate.Parameters.Add(New SqlParameter("@password", tbPassword.Text))
+
+		drAuthenticate = cmdAuthenticate.ExecuteReader()
+		If drAuthenticate.Read() Then
+			lblLogin.Text = "Welcome! "
+			sqlDSGradesUser.SelectCommand = "SELECT * FROM Grades WHERE StudentID = " & drAuthenticate.Item("StudentID")
+			sqlDSGradesUser.DataBind()
+			gvGradesUser.DataBind()
+			sqlDSStudentsAll.SelectCommand = "SELECT * FROM Students"
+			gvStudentsAll.DataBind()
+			sqlDSGradesAll.SelectCommand = "SELECT * FROM Grades"
+			gvGradesAll.DataBind()
+			gvGradesUser.Visible = True
+			gvStudentsAll.Visible = True
+			gvGradesAll.Visible = True
+			lblGradesUser.Visible = True
+			lblStudentsAll.Visible = True
+			lblGradesAll.Visible = True
+			lblSQLStatement.Visible = True
+			lblSQL.Text = strSQL
+		Else
+			lblLogin.Text = "Invalid credential."
+		End If
+	End Sub
 
     Protected Sub btnReset_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnReset.Click
         tbUsername.Text = ""
